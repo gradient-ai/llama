fa7696a3d9142fe51e93a132c8510b8b3011a392
jameshskelton <jameshskelton@gmail.com>
2023-03-07 14:59:26 -0600

    added new gradio application

--- example.py
+++ example.py
@@ -8,15 +8,23 @@ import torch
 import fire
 import time
 import json
+import argparse
 
 from pathlib import Path
 
 from fairscale.nn.model_parallel.initialize import initialize_model_parallel
 
 from llama import ModelArgs, Transformer, Tokenizer, LLaMA
+import torch
+
+def seedTorch(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
-def setup_model_parallel() -> Tuple[int, int]:
+def setup_model_parallel(seed) -> Tuple[int, int]:
     local_rank = int(os.environ.get("LOCAL_RANK", -1))
     world_size = int(os.environ.get("WORLD_SIZE", -1))
 
@@ -25,9 +33,8 @@ def setup_model_parallel() -> Tuple[int, int]:
     torch.cuda.set_device(local_rank)
 
     # seed must be the same in all processes
-    torch.manual_seed(1)
+    torch.manual_seed(seed)
     return local_rank, world_size
-
 
 def load(
     ckpt_dir: str,
@@ -39,6 +46,7 @@ def load(
 ) -> LLaMA:
     start_time = time.time()
     checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    print(checkpoints)
     assert world_size == len(
         checkpoints
     ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
@@ -54,11 +62,14 @@ def load(
     tokenizer = Tokenizer(model_path=tokenizer_path)
     model_args.vocab_size = tokenizer.n_words
     torch.set_default_tensor_type(torch.cuda.HalfTensor)
-    model = Transformer(model_args)
+    model = Transformer(model_args).cuda().half()
+
     torch.set_default_tensor_type(torch.FloatTensor)
     model.load_state_dict(checkpoint, strict=False)
 
     generator = LLaMA(model, tokenizer)
+    
+
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
     return generator
 
@@ -66,12 +77,14 @@ def load(
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
+    seed: int,
+    prompts: str,
     temperature: float = 0.8,
     top_p: float = 0.95,
     max_seq_len: int = 512,
-    max_batch_size: int = 32,
-):
-    local_rank, world_size = setup_model_parallel()
+    max_batch_size: int = 16):
+
+    local_rank, world_size = setup_model_parallel(seed)
     if local_rank > 0:
         sys.stdout = open(os.devnull, "w")
 
@@ -79,36 +92,15 @@ def main(
         ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
     )
 
-    prompts = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        "Building a website can be done in 10 simple steps:\n",
-        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
-        """Tweet: "I hate it when my phone battery dies."
-Sentiment: Negative
-###
-Tweet: "My day has been 👍"
-Sentiment: Positive
-###
-Tweet: "This is the link to the article"
-Sentiment: Neutral
-###
-Tweet: "This new music video was incredibile"
-Sentiment:""",
-        """Translate English to French:
+   
+    prompts = [prompts]
 
-sea otter => loutre de mer
-
-peppermint => menthe poivrée
-
-plush girafe => girafe peluche
-
-cheese =>""",
-    ]
+    
     results = generator.generate(
-        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+        prompts, max_gen_len=40000, temperature=temperature, top_p=top_p
     )
+    
+    return results
 
     for result in results:
         print(result)
@@ -117,3 +109,4 @@ cheese =>""",
 
 if __name__ == "__main__":
     fire.Fire(main)
+
